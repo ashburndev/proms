299a593aeb043830627f8da0b85fa244e5c33e27
ashburndev <ashburndev@gmail.com>
2023-10-26 09:22:36 -0400

    first commit

---
+++ src/main/java/com/ashburncode/proms/PromsApplication.java
@@ -0,0 +1,13 @@
+package com.ashburncode.proms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashburncode/proms/PromsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashburncode.proms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
